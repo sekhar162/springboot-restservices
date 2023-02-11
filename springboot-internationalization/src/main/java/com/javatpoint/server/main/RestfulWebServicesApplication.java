93cf7ed88349834bf354d20660d61607865bf82e
Reddi Sekhar Thammisetti <sekhar9216@gmail.com>
2023-02-11 16:16:04 +0530

    springboot internationalization added

---
+++ springboot-internationalization/src/main/java/com/javatpoint/server/main/RestfulWebServicesApplication.java
@@ -0,0 +1,32 @@
+package com.javatpoint.server.main;
+
+import java.util.Locale;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+@SpringBootApplication
+public class RestfulWebServicesApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulWebServicesApplication.class, args);
+	}
+
+//configuring default locale
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
+	}
+
+//configuring ResourceBundle
+	@Bean
+	public ResourceBundleMessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("messages");
+		return messageSource;
+	}
+}
